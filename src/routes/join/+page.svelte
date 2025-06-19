5afb76ac6340ba2e99331b33bf60582e8e6c0d6a
CCRAstra <121424228+CCRAstra@users.noreply.github.com>
2025-06-19 02:11:21 -0500

    Disable Applications
    Old discord invite was also made invalid

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -4,11 +4,11 @@
 
 <div class="mx-auto mt-8 flex w-full max-w-7xl flex-col items-center justify-center px-4 sm:px-6">
 	<h1 class="oswald-font text-2xl font-black underline sm:text-4xl">How to Join</h1>
-<p class="mt-4 text-center text-base text-gray-400 sm:text-lg">We are not accepting new members at this time</p>
+<p class="mt-4 text-center text-base text-gray-400 sm:text-lg">CCR Clan has stopped accepting applications due to application flooding.</p>
 	<Button
-		href="https://discord.gg/5DqAzjSh9C"
+		href="#"
 		variant="secondary"
-		class="mt-8 p-6 text-lg font-light underline sm:p-10 sm:text-3xl">Apply in the Discord!</Button
+		class="mt-8 p-6 text-lg font-light underline sm:p-10 sm:text-3xl">Applications Disabled</Button
 	>
 </div>
 
