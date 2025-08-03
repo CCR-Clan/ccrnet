224dbaaafd2c7d609eee3111d1d485a61740bcd7
CCRAstra <121424228+CCRAstra@users.noreply.github.com>
2025-08-03 14:25:09 -0500

    Update +page.svelte

--- src/routes/minecraft/+page.svelte
+++ src/routes/minecraft/+page.svelte
@@ -3,8 +3,9 @@
 </script>
 
 <div class="mx-auto mt-8 flex w-full max-w-7xl flex-col items-center justify-center px-4 sm:px-6">
-	<h1 class="oswald-font text-2xl font-black sm:text-4xl">Minecraft Server</h1>
-	<p class="mt-4 text-center text-base text-gray-400 sm:text-lg">The realm beta is now open!  There are issues with the realm server causing extreme lag when more player come online.. I'm trying to fix it. If you would like to participate in the beta, please dm "ccrastra" on discord!</p>
+	<h1 class="oswald-font text-2xl font-black sm:text-4xl">CCR Craft</h1>
+<h3 class="oswald-font text-2xl font-black sm:text-4xl">Season 3: Better on Bedrock</h3>
+	<p class="mt-4 text-center text-base text-gray-400 sm:text-lg">The realm beta is now open!  There are issues with the realm server causing extreme lag when more players are online, this is being worked on. If you would like to participate in the beta, please dm "ccrastra" on discord!</p>
 </div>
 
 <style>
