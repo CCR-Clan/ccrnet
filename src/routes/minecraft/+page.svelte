35a35677eb31f7aab0e0a28e7399141e79e08780
CCRAstra <121424228+CCRAstra@users.noreply.github.com>
2025-08-03 14:22:21 -0500

    Update +page.svelte

--- src/routes/minecraft/+page.svelte
+++ src/routes/minecraft/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="mx-auto mt-8 flex w-full max-w-7xl flex-col items-center justify-center px-4 sm:px-6">
 	<h1 class="oswald-font text-2xl font-black sm:text-4xl">Minecraft Server</h1>
-	<p class="mt-4 text-center text-base text-gray-400 sm:text-lg">Unfortunatly, we had to close the CCR Realm because we are broke and can't pay for it all the time. This page will update if the realm becomes available again.</p>
+	<p class="mt-4 text-center text-base text-gray-400 sm:text-lg">The realm beta is now open!  There are issues with the realm server causing extreme lag when more player come online.. I'm trying to fix it. If you would like to participate in the beta, please dm "ccrastra" on discord!</p>
 </div>
 
 <style>
